f551f56b6ec1aef871a99c34e90510c9eba1a688
Sue Holder <susanpholder@gmail.com>
2020-03-31 17:53:21 +0100

    update os

--- app.py
+++ app.py
@@ -8,5 +8,7 @@ def hello():
     return 'Hello World...again'
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+            port=int(os.environ.get('PORT', '5000')),
+            debug=True)
