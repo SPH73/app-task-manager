fc2d5820a0145b866cb217335e226e9a5ef423a1
Sue Holder <susanpholder@gmail.com>
2020-04-05 19:40:21 +0100

    added edittask.html, task buttons and styling

--- app.py
+++ app.py
@@ -18,6 +18,26 @@ def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
+@app.route('/add_task')
+def add_task():
+    return render_template("addtask.html", categories=mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+
+
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task,
+                           categories=all_categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=int(os.environ.get('PORT', '5000')),
